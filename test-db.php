f7c3512dbbbd71862e4f7038f2be5e3094466d35
Mathys <M.PeypouxDev@gmail.com>
2025-10-03 11:21:53 +0200

    test db users

---
+++ test-db.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'config/database.php';
+
+$database = new Database();
+$conn = $database->getConnection();
+
+if ($conn) {
+    echo "Connexion réussie !";
+
+    // Test des données
+    $stmt = $conn->query("SELECT * FROM users");
+    $users = $stmt->fetchAll();
+
+    echo "Nombres d'utilisateurs : " . count($users) . ;
+    foreach($users as $user) {
+        echo "- {$user['prenom']} {$user['nom']} ({$user['email']})";
+    }
+} else {
+    echo "Erreur de connexion";
+}
